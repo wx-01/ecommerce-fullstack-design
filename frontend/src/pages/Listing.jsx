9006b1477b2040df717350212d487f7d7d9e9fd5
Wajid Ali <94067298+wx-01@users.noreply.github.com>
2025-06-11 22:25:13 +0500

    listing page started

--- frontend/src/pages/Listing.jsx
+++ frontend/src/pages/Listing.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import NewsLetter from "../components/NewsLetter";
 
 const Listing = () => {
   return (
-    <div>Listing</div>
-  )
-}
+    <>
+      <main className="pt-19 md:relative  flex flex-col items-center justify-center md:pt-27  md:ml-18 md:mr-18 lg:ml-26 lg:mr-26">
+        <div className="w-full">
+          {/* top */}
+          <div className="w-full flex items-center justify-start py-3 text-gray-400 ">
+            <h1>Home &gt; Clothings &gt; Men's Wear &gt; Summer Clothing</h1>
+          </div>
+          <div className="flex flex-row">
+            {/* left section */}
+            <div >
+           
+           
+            </div>
+            {/* right section */}
+            <div>
+              {/* header */}
+              <div></div>
+              {/* items */}
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <NewsLetter />
+    </>
+  );
+};
 
-export default Listing
+export default Listing;
